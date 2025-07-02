39d9eab01749b6dea405f5e6b4ff39ff525195de
Chandan Chatarjee <bimalchatarjee159@gmail.com>
2025-07-02 12:25:42 +0600

    Create binaryTree.cpp
    Binary tree algorithm

---
+++ binaryTree.cpp
@@ -0,0 +1,65 @@
+#include<bits/std.c++a.out.h>ysing namespace std;
+#define ll long long
+
+class Node{
+  public:
+  int data;
+  Node* left;
+  Node* right;
+
+  Node(int val){
+    data=val;
+    left=right=NULL;
+  }
+};
+
+
+//build tree from preorder 
+static int idx=-1;
+Node* buildTree(vector<ll> preorder){
+  idx++;
+  if(preorder[idx]==-1){
+    return NULL;
+  }
+  Node* root=new Node(preorder[idx]);
+  root-> left=buildTree(int preorder) //left subtree
+  root->right=buildTree(int preorder) //right subtree
+  return root;
+}
+
+
+// preorder traversal
+void preOrder(Node* root){
+  if(root==NULL){
+    return;
+  }
+  cout<<root->data<<" ";
+  preOrder(root->left);
+  preOrder(root->right);
+  
+}
+
+
+// inorder traversal
+void inOrder(Node* root){
+  if(root==NULL){
+    return;
+  }
+  inOrder(root->left);
+  cout<<root->data<<" ";
+  inOrder(root->right);
+  
+}
+  
+
+
+
+int main(){
+  vector<ll> preorder={1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+  Node* root=buildTree(preorder);
+  preOrder(root);
+  cout<<endl;
+  
+
+  return 0;
+}
