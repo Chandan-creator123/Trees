b33ed16c09e238b8daecfcabf7313f2c18af3987
Chandan Chatarjee <bimalchatarjee159@gmail.com>
2025-07-02 12:42:29 +0600

    Update binaryTree.cpp

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -1,27 +1,29 @@
 #include<bits/std.c++a.out.h>ysing namespace std;
 #define ll long long
 
+
 class Node{
   public:
-  int data;
-  Node* left;
-  Node* right;
+  int data; // data
+  Node* left;// left child
+  Node* right;// right child
 
-  Node(int val){
-    data=val;
-    left=right=NULL;
+  Node(int val){ // constructor. Creating because we need to create new node every time
+    data=val; // data
+    left=right=NULL;// left and right child. initially NULL. then we will assign left and right child
   }
 };
+// we are creating it because we need to create new node every time. So we need to create a constructor.
 
 
 //build tree from preorder 
 static int idx=-1;
 Node* buildTree(vector<ll> preorder){
-  idx++;
+  idx++; // increment idx
   if(preorder[idx]==-1){
     return NULL;
   }
-  Node* root=new Node(preorder[idx]);
+  Node* root=new Node(preorder[idx]); // root node. Means first element of preorder
   root-> left=buildTree(int preorder) //left subtree
   root->right=buildTree(int preorder) //right subtree
   return root;
@@ -50,6 +52,38 @@ void inOrder(Node* root){
   inOrder(root->right);
   
 }
+
+// postorder traversal
+void postOrder(Node* root){
+  if(root==NULL){
+    return;
+  }
+  postOrder(root->left); //left subtree
+  postOrder(root->right);//right subtree
+  cout<<root->data<<" ";//root
+}
+
+// level order traversal
+void levelOrder(Node* root){
+  if(root==NULL){
+    return;
+  }
+  queue<Node*> q;
+  q.push(root);
+  while(!q.empty()){ // q.size()!=0
+    Node* current=q.front(); // first element . Means root && then left && then right
+    q.pop();// remove first element
+    cout<<current->data<<" ";// print first element
+    if(current->left!=NULL){
+      q.push(current->left); // push left child
+    }
+    if(current->right!=NULL){
+      q.push(current->right);// push right child
+    }
+    
+  }
+  
+}
   
 
 
@@ -57,9 +91,22 @@ void inOrder(Node* root){
 int main(){
   vector<ll> preorder={1,2,-1,-1,3,4,-1,-1,5,-1,-1};
   Node* root=buildTree(preorder);
+
+  // preorder traversal
   preOrder(root);
   cout<<endl;
-  
 
+  // inorder traversal
+  inOrder(root);
+  cout<<endl;
+
+  // postorder traversal
+  postOrder(root);
+
+  // level order traversal
+  levelOrder(root);
+  cout<<endl;
+
+  
   return 0;
 }
